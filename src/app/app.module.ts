6d202aa3f7773e88218d6be686f2c01515be8fbf
AK <kasssalex@gmail.com>
2018-01-28 01:13:33 +0100

    use library components for menu and main application header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -171,9 +171,6 @@ import { ChartService } from './Shared/Services/chart.service'
 import { AuthAnoynmousService } from './Shared/Services/auth-anonymous.service'
 import { TranslationLoaderService, TranslationServicesModule } from 'gg-translation'
 
-import { MenuComponent } from './menu.component'
-
-
 import { TranslateModule, TranslateLoader, TranslateService  } from '@ngx-translate/core'
 //import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
@@ -282,7 +279,7 @@ registerLocaleData(localeFr, 'fr');  // https://angular.io/guide/i18n#i18n-pipes
       { path: '**', redirectTo: '/home' }
     ])
   ],
-  declarations: [AppComponent, HomeComponent,          MenuComponent,
+  declarations: [AppComponent, HomeComponent,         
     VoucherListComponent, VoucherDetailComponent, MonitoringDetailComponent,
     AdminMainComponent, AdminWebShoppingComponent, AdminWebShoppingVoucherRequestListComponent, AdminWebShoppingVoucherRequestComponent, AdminAK, AdminLabo,
     LaboratoryEnterComponent, CurrencyEnterComponent,
