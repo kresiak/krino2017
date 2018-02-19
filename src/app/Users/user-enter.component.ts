5fa624d7b6f25b7258b155494dd80a2341946681
AK <kasssalex@gmail.com>
2018-02-19 09:39:32 +0100

    Idem for user enter

--- src/app/Users/user-enter.component.ts
+++ src/app/Users/user-enter.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { DataStore } from 'gg-basic-data-services';
 import { NotificationService } from './../Shared/Services/notification.service';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormItemStructure, FormItemType} from 'gg-ui'
+
 
 @Component(
     {
@@ -12,56 +13,44 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 export class UserEnterComponent implements OnInit {
     
-    public newUserForm: FormGroup;    
-
-    constructor(private formBuilder: FormBuilder, private dataStore: DataStore, private notificationService: NotificationService ) {}
+    public formStructure: FormItemStructure[]= []
+    
+    constructor(private dataStore: DataStore, private notificationService: NotificationService ) {}
 
     ngOnInit(): void {
-        // todo: ne marche pas encore
-        //  const emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
-
-            const emailRegex = /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}\s*$/i;
-
-        this.newUserForm = this.formBuilder.group({                      
-            name: ['', [Validators.required, Validators.minLength(3)]],
-            firstName: ['', [Validators.required, Validators.minLength(3)]],
-            email: ['', [Validators.required, Validators.pattern(emailRegex)]],
-            isBlocked: [''],
-            isLaboUser: [''],
-            isAdmin: [''],
-            password: [''],
-            isReceptionist: [''],
-            isLabManager: [''],
-            isProgrammer: [''],
-            isPassiveUser: ['']
-        });
+        this.formStructure.push(new FormItemStructure('name', 'USER.LABEL.NAME', FormItemType.InputText, {isRequired: true, minimalLength: 2, placeholderKey:'USER.LABEL.NAME OF THE USER PHOLDER'}))
+        this.formStructure.push(new FormItemStructure('firstName', 'USER.LABEL.FIRST NAME', FormItemType.InputText, {isRequired: true, minimalLength: 2, placeholderKey:'USER.LABEL.FIRST NAME OF THE USER PHOLDER'}))
+        this.formStructure.push(new FormItemStructure('email', 'USER.LABEL.EMAIL', FormItemType.InputText, {isRequired: true, isEmail:true}))
+        this.formStructure.push(new FormItemStructure('password', 'USER.LABEL.PASSWORD', FormItemType.InputText, {isRequired: true, minimalLength: 2}))
+        
+        this.formStructure.push(new FormItemStructure('isBlocked', 'USER.LABEL.IS BLOCKED', FormItemType.InputCheckbox))
+        this.formStructure.push(new FormItemStructure('isLaboUser', 'USER.LABEL.IS LABO USER', FormItemType.InputCheckbox))
+        this.formStructure.push(new FormItemStructure('isAdmin', 'USER.LABEL.IS ADMINISTRATOR', FormItemType.InputCheckbox))
+        this.formStructure.push(new FormItemStructure('isReceptionist', 'USER.LABEL.IS RECEPTIONIST', FormItemType.InputCheckbox))
+        this.formStructure.push(new FormItemStructure('isLabManager', 'USER.LABEL.IS LAB MANAGER', FormItemType.InputCheckbox))
+        this.formStructure.push(new FormItemStructure('isProgrammer', 'USER.LABEL.IS KRINO PROGRAMMER', FormItemType.InputCheckbox))
+        this.formStructure.push(new FormItemStructure('isPassiveUser', 'USER.LABEL.USER IS NOT ALLOWED TO ORDER PRODUCTS', FormItemType.InputCheckbox))
     };
     
-    save(formValue, isValid)
+    formSaved(data)
     {
         this.dataStore.addData('users.krino', {
-            name: formValue.name,
-            firstName: formValue.firstName,
-            email: formValue.email.trim(),
-            isBlocked: formValue.isBlocked!=='' && formValue.isBlocked!==null,
-            isLaboUser: formValue.isLaboUser!=='' && formValue.isLaboUser!==null,
-            isAdmin: formValue.isAdmin!=='' && formValue.isAdmin!==null,
-            password: formValue.password,
-            isReceptionist: formValue.isReceptionist!=='' && formValue.isReceptionist!==null,
-            isLabManager: formValue.isLabManager!=='' && formValue.isLabManager!==null,
-            isProgrammer: formValue.isProgrammer!=='' && formValue.isProgrammer!==null,
-            isPassiveUser: formValue.isPassiveUser!=='' && formValue.isPassiveUser!==null
+            name: data.name,
+            firstName: data.firstName,
+            email: data.email,
+            isBlocked: data.isBlocked,
+            isLaboUser: data.isLaboUser,
+            isAdmin: data.isAdmin,
+            password: data.password,
+            isReceptionist: data.isReceptionist,
+            isLabManager: data.isLabManager,
+            isProgrammer: data.isProgrammer,
+            isPassiveUser: data.isPassiveUser
         }).first().subscribe(res =>
         {
-            var x=res;
-            this.reset();
-            this.notificationService.mailTo(formValue.email.trim(), 'A Krino account has been created for you', '<p>Your password is ' + formValue.password + '</p><p>You can find Krino on http://139.165.57.34/krino/home or <a href="http://139.165.57.34/krino/home">Click here</a></p>' )
+            data.setSuccess('OK')
+            this.notificationService.mailTo(data.email.trim(), 'A Krino account has been created for you', '<p>Your password is ' + data.password + '</p><p>You can find Krino on http://139.165.57.34/krino/home or <a href="http://139.165.57.34/krino/home">Click here</a></p>' )
         });
-    }
-
-    reset()
-    {
-        this.newUserForm.reset();    
     }
 
 
